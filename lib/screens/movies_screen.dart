425f6da6c401c928026bec3dbf03dc9141389db9
Julio <julioantonini@gmail.com>
2024-12-14 18:18:07 -0300

    feat: create screens

--- lib/screens/movies_screen.dart
+++ lib/screens/movies_screen.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/movies/movies_widget.dart';
+
 class MoviesScreen extends StatelessWidget {
   const MoviesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text("Hello world"),
-    ));
+      appBar: AppBar(
+        title: Text("Popular movies"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite_rounded,
+              color: Colors.red,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.dark_mode,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return MoviesWidget();
+        },
+      ),
+    );
   }
 }
