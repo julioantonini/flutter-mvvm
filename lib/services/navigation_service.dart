cf063dc9b32e23a96fe12d9b3d08a54782c68e05
Julio <julioantonini@gmail.com>
2024-12-15 15:47:20 -0300

    feat: create dialog and snackbar

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -11,4 +11,24 @@ class NavigationService {
     return navigatorKey.currentState
         ?.push(MaterialPageRoute(builder: (context) => widget));
   }
+
+  Future<void> showDialog(Widget widget) async {
+    await showAdaptiveDialog(
+        barrierDismissible: true,
+        context: navigatorKey.currentContext!,
+        builder: (context) => widget);
+  }
+
+  void showSnackbar() {
+    final context = navigatorKey.currentContext!;
+    ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+    const snackbarWidget = SnackBar(
+        content: Text(
+      "Hello World - a movies app",
+      style: TextStyle(color: Colors.white),
+    ));
+    ScaffoldMessenger.of(context).showSnackBar(
+      snackbarWidget,
+    );
+  }
 }
